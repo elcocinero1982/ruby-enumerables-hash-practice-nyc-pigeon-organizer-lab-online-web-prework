29804b25341176f1f9e5d962680303fc78639ff6
jose luis <cortezluisjose@gmail.com>
2019-07-06 03:02:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -38,18 +38,6 @@ def nyc_pigeon_organizer(data)
   }
 }
 
-pigeon_data = {
-  :color => {
-    :brown => ["Luca"],
-    :black => ["Lola"],
-  },
-  :gender => {
-    :male => ["Luca"],
-    :female => ["Lola"]
-  },
-  :lives => {
-    "Central Park" => ["Lola"],
-    "Library" => ["Luca"]
-  }
-}
+
+
 end
